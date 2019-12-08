3e5cd89ec74e3102be5e27943fc0bd8f70b31d36
Oscar Synnemar <oscar@synnemar.com>
2019-12-08 14:56:04 +0100

    Updated drawer styling

--- src/components/drawer.tsx
+++ src/components/drawer.tsx
@@ -11,14 +11,11 @@ const Overlay = styled.div`
   left: 0;
   background-color: rgb(169, 23, 23, 1);
   overflow-x: hidden;
-  transition: 0.05s;
   a {
-    font-weight: bold;
     font-family: "Roboto", sans-serif;
-    padding: 16px;
+    padding: 4px;
     text-decoration: none;
-    font-size: 36px;
-    color: #FFF;
+    color: #fff;
     display: block;
     transition: 0.3s;
     &:hover,
@@ -30,36 +27,38 @@ const Overlay = styled.div`
 
 const OverlayContent = styled.div`
   position: relative;
-  top: 10%;
-  width: 95%;
+  top: 5%;
   text-align: right;
   margin-top: 30px;
+  margin-right: 60px;
 `
 
 const HeadLink = styled(Link)`
-font-size: 30px;
-color: blue;
+  
+  font-size: 30px;
+  font-weight: bold;
+  margin-top: 8px;
 `
 const SubLink = styled(Link)`
-font-size: 20px;
+  font-size: 20px;
 `
 
 const drawer: React.FC<{ isOpen: boolean }> = ({ isOpen }) => {
   return (
     <Overlay width={isOpen ? "100%" : "0%"}>
-      <OverlayContent >
+      <OverlayContent>
         <HeadLink to="/">Home</HeadLink>
         <HeadLink to="/">For New Students</HeadLink>
-          <SubLink to="/">Events</SubLink>
-          <SubLink to="/">Guide for new students</SubLink>
+        <SubLink to="/">Events</SubLink>
+        <SubLink to="/">Guide for new students</SubLink>
         <HeadLink to="/">About us</HeadLink>
-          <SubLink to="/">Mission & Vision</SubLink>
-          <SubLink to="/">Archive</SubLink>
+        <SubLink to="/">Mission & Vision</SubLink>
+        <SubLink to="/">Archive</SubLink>
         <HeadLink to="/">The Team</HeadLink>
-          <SubLink to="/">Buddies</SubLink>
-          <SubLink to="/">Project Team</SubLink>
-          <SubLink to="/">Project Manager</SubLink>
-          <SubLink to="/">Join Us</SubLink>
+        <SubLink to="/">Buddies</SubLink>
+        <SubLink to="/">Project Team</SubLink>
+        <SubLink to="/">Project Manager</SubLink>
+        <SubLink to="/">Join Us</SubLink>
         <HeadLink to="/">News</HeadLink>
         <HeadLink to="/">Contact</HeadLink>
         <HeadLink to="/">Partners</HeadLink>
