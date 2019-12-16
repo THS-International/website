4ac19d8fab89390c21a6cae96f46409ee207f3e3
Alexander Brokking <brokki@kth.se>
2019-12-16 15:38:17 +0100

    News Generator

--- src/pages/news.tsx
+++ src/pages/news.tsx
@@ -3,6 +3,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import styled from "styled-components"
+import NewsGenerator from "../components/NewsGenerator"
 
 const Title = styled.div`
 font-size: 36px;
@@ -16,6 +17,9 @@ const News: React.FC = () => (
   <Layout>
     <SEO title="Join" />
     <h1>News</h1>
+
+    <NewsGenerator limit="all"></NewsGenerator>
+
   </Layout>
 )
 
