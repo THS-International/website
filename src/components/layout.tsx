e82e54bd44e4c8285c27298cd9bbedc2c524a0e0
Oscar Synnemar <oscar@synnemar.com>
2019-12-17 22:10:25 +0100

    Made the website a little better for desktop view

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,8 +1,23 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Header from "./header"
-import './normalize.css'
+import "./normalize.css"
 import styled from "styled-components"
+
+const Footer = styled.footer`
+  background-color: #750505;
+  height: 3rem;
+  padding: 1rem;
+  font-size: 12px;
+  font-weight: 600;
+  align-self: center;
+  color: rgba(255, 255, 255, 0.9);
+`
+
+const Main = styled.main`
+  margin: 0 auto;
+  max-width: 960px;
+`
 
 const Layout: React.FC = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -15,34 +30,11 @@ const Layout: React.FC = ({ children }) => {
     }
   `)
 
-const Footer = styled.footer`
-background-color: #750505;
-height: 3rem;
-padding: 1rem;
-font-size: 12px;
-font-weight: 600;
-align-self: center;
-color: rgba(255,255,255,.9);
-
-`
-
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          maxWidth: 960,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        {/* <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer> */}
-        <Footer>International reception 2019</Footer>
-      </div>
+      <Main>{children}</Main>
+      <Footer>International reception 2019</Footer>
     </>
   )
 }
