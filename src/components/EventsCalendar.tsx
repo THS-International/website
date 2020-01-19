950976cdc86abea05b89736b72c30a142ab7ed35
Alexander Brokking <brokki@kth.se>
2020-01-19 13:25:09 +0100

    cleaned up eventsCalender code

--- src/components/EventsCalendar.tsx
+++ src/components/EventsCalendar.tsx
@@ -4,7 +4,6 @@ import dayGridPlugin from "@fullcalendar/daygrid"
 import timeGridPlugin from "@fullcalendar/timegrid"
 import googleCalendarPlugin from "@fullcalendar/google-calendar"
 import styled from "styled-components"
-// import Exit from "../images/vector.svg"
 
 import "@fullcalendar/core/main.css"
 import "@fullcalendar/daygrid/main.css"
@@ -25,7 +24,7 @@ const ED = styled.div`
     top: -312px;
     right: 10px;
     width: 12px;
-    /* opacity: .8; */
+    opacity: .8;
 
     :hover {
       opacity: 1;
@@ -34,16 +33,16 @@ const ED = styled.div`
 `
 
 const Description = styled.div`
-font-size: 14px;
+  font-size: 14px;
 `
 
 const Time = styled.div`
-font-weight: 300;
-font-size: 14px;
+  font-weight: 300;
+  font-size: 14px;
 `
 
 const EDtitle = styled.div`
-font-weight: 600;
+  font-weight: 600;
 `
 
 const EventsCalendar = () => {
@@ -52,17 +51,16 @@ const EventsCalendar = () => {
   const [FocEvent, setFocEvent] = useState({
     title: "title",
     description: "price",
-    Time: "8am"
+    Time: "8am",
   })
-
 
   const details = info => {
     let xpos = info.el.getBoundingClientRect().left + window.scrollX
     let ypos = info.el.getBoundingClientRect().bottom + window.scrollY
-    if (xpos + screen.width/2 > screen.width) {
+    if (xpos + screen.width / 2 > screen.width) {
       xpos -= 180
     }
-    if (ypos + 100 > screen.height){
+    if (ypos + 100 > screen.height) {
       ypos -= 140
     }
 
@@ -70,24 +68,46 @@ const EventsCalendar = () => {
     setFocEvent({
       title: info.event.title,
       description: parseInt(info.event.extendedProps.description).toString(),
-      Time: info.event.start.getDate() + ": " + info.event.start.toTimeString().substring(0,5) + " - " + info.event.end.toTimeString().substring(0,5)
+      Time:
+        info.event.start.toDateString().substring(4,10) +
+        ": " +
+        info.event.start.toTimeString().substring(0, 5) +
+        " - " +
+        info.event.end.toTimeString().substring(0, 5),
     })
     setShow("block")
   }
 
-  useEffect(() => { if(window.innerWidth > 966) setShow("none") }, [Show])
+  useEffect(() => {
+    if (window.innerWidth > 966) setShow("none")
+  }, [Show])
 
   return (
     <>
-      <ED style={{ left: Pos[0], top: Pos[1], display: Show}}>
+      <ED style={{ left: Pos[0], top: Pos[1], display: Show }}>
         <EDtitle>{FocEvent.title}</EDtitle>
         <Time>{FocEvent.Time}</Time>
         <Description>Price: {FocEvent.description}kr</Description>
-        {/* <img onClick={() => setShow("none")} src={Exit} style={window.innerWidth > 966 ? {display: "none"} : {display: "block"}} alt="Logo"/> */}
-        <svg onClick={() => setShow("none")} style={window.innerWidth > 966 ? {display: "none"} : {display: "block"}} width="655" height="655" viewBox="0 0 655 655" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M12.2841 638.781C-3.33688 623.16 -3.33688 597.833 12.2841 582.212L582.212 12.2844C597.833 -3.33659 623.16 -3.33657 638.781 12.2844L643.023 16.527C658.644 32.148 658.644 57.4746 643.023 73.0956L73.0953 643.024C57.4743 658.645 32.1477 658.645 16.5267 643.024L12.2841 638.781Z" fill="#4A4A4A"/>
-<path d="M16.5268 12.2843C32.1477 -3.33664 57.4743 -3.33664 73.0953 12.2843L643.023 582.212C658.644 597.833 658.644 623.16 643.023 638.781L638.781 643.024C623.16 658.645 597.833 658.645 582.212 643.024L12.2841 73.0955C-3.33686 57.4745 -3.33684 32.1479 12.2841 16.527L16.5268 12.2843Z" fill="#4A4A4A"/>
-      </svg>
+        <svg
+          onClick={() => setShow("none")}
+          style={
+            window.innerWidth > 966 ? { display: "none" } : { display: "block" }
+          }
+          width="655"
+          height="655"
+          viewBox="0 0 655 655"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M12.2841 638.781C-3.33688 623.16 -3.33688 597.833 12.2841 582.212L582.212 12.2844C597.833 -3.33659 623.16 -3.33657 638.781 12.2844L643.023 16.527C658.644 32.148 658.644 57.4746 643.023 73.0956L73.0953 643.024C57.4743 658.645 32.1477 658.645 16.5267 643.024L12.2841 638.781Z"
+            fill="#4A4A4A"
+          />
+          <path
+            d="M16.5268 12.2843C32.1477 -3.33664 57.4743 -3.33664 73.0953 12.2843L643.023 582.212C658.644 597.833 658.644 623.16 643.023 638.781L638.781 643.024C623.16 658.645 597.833 658.645 582.212 643.024L12.2841 73.0955C-3.33686 57.4745 -3.33684 32.1479 12.2841 16.527L16.5268 12.2843Z"
+            fill="#4A4A4A"
+          />
+        </svg>
       </ED>
 
       <FullCalendar
@@ -105,14 +125,13 @@ const EventsCalendar = () => {
           info.jsEvent.preventDefault()
           details(info)
         }}
-        eventMouseEnter={ info => {
+        eventMouseEnter={info => {
           details(info)
         }}
-        eventMouseLeave={ () => {
+        eventMouseLeave={() => {
           setShow("none")
         }}
-        
-        />
+      />
     </>
   )
 }
