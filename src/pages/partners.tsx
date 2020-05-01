d6c47d06f24a851ed854c3a025f6f8d749718c07
Alexander Brokking <alexander.balfors@gmail.com>
2020-05-01 15:31:24 +0200

    Created partners page

---
+++ src/pages/partners.tsx
@@ -0,0 +1,26 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { graphql, useStaticQuery } from "gatsby"
+
+const query = graphql`
+  query {
+    markdownRemark(frontmatter: { type: { eq: "partners-page" } }) {
+      html
+    }
+  }
+`
+
+const Pt: React.FC = () => {
+  const queryResult = useStaticQuery(query)
+  return (
+    <Layout>
+      <SEO title="Partners" />
+      <h1>Partners</h1>
+      <div dangerouslySetInnerHTML={{__html: queryResult.markdownRemark.html}} />
+    </Layout>
+  )
+}
+
+export default partners
